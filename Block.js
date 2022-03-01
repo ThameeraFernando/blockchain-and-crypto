142f22b61733412e98d2ebc4176f7ee54d63799b
ThameeraFernando <sithnuwan111@gmail.com>
2022-03-01 20:46:24 +0530

    genesis block method and test

--- Block.js
+++ Block.js
@@ -1,9 +1,16 @@
+const { GENESIS_DATA } = require("./config");
+
 class Block {
+  //constructor
   constructor({ timestamp, lasthash, hash, data }) {
     this.timestamp = timestamp;
     this.lasthash = lasthash;
     this.hash = hash;
     this.data = data;
+  }
+  //get genesis block method
+  static genesis() {
+    return new Block(GENESIS_DATA);
   }
 }
 module.exports = Block;
